ad100987d1b6d9ed3de7c57061bfa3f5a561a519
Dainese Hsiao <daineseh@gmail.com>
2015-06-06 15:56:19 +0800

    [Mjolnir] Add to split the quadrant screen of key binding

--- mjolnir/.mjolnir/init.lua
+++ mjolnir/.mjolnir/init.lua
@@ -6,6 +6,7 @@ local fnutils = require "mjolnir.fnutils"
 -- mjolnir.alert.show("mjolnir config loaded", 1.5)
 local mash = {"ctrl", 'alt'}
 local mashshift = {"cmd", "alt", "shift"}
+local kb = {"cmd", "alt"}
 
 -- move the window to the right half of the screen
 hotkey.bind({"cmd"}, "Right", function()
@@ -33,3 +34,43 @@ hotkey.bind({"cmd"}, "Up", function()
     win:setframe(newframe)
 end)
 
+-- move the window to the quadrant 1 of the screen
+hotkey.bind(kb, "P", function()
+    local win = window.focusedwindow()
+    local newframe = win:screen():frame()
+    newframe.w = newframe.w/2
+    newframe.x = newframe.w -- comment this line to push it to left of screen
+    newframe.h = newframe.h/2
+    newframe.y = newframe.y
+    win:setframe(newframe)
+end)
+
+-- move the window to the quadrant 2 of the screen
+hotkey.bind(kb, "Q", function()
+    local win = window.focusedwindow()
+    local newframe = win:screen():frame()
+    newframe.w = newframe.w/2
+    newframe.h = newframe.h/2
+    win:setframe(newframe)
+end)
+
+-- move the window to the quadrant 3 of the screen
+hotkey.bind(kb, "/", function()
+    local win = window.focusedwindow()
+    local newframe = win:screen():frame()
+    newframe.w = newframe.w/2
+    newframe.x = newframe.w
+    newframe.h = newframe.h/2
+    newframe.y = newframe.h
+    win:setframe(newframe)
+end)
+
+-- move the window to the quadrant 4 of the screen
+hotkey.bind(kb, "Z", function()
+    local win = window.focusedwindow()
+    local newframe = win:screen():frame()
+    newframe.w = newframe.w/2
+    newframe.h = newframe.h/2
+    newframe.y = newframe.h
+    win:setframe(newframe)
+end)
